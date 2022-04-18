b11d88eef6bb4b0979033e85d8fa8803e7f77a2a
juliana teixeira <teixeirajulianakellys@gmail.com>
2022-04-18 15:04:16 -0300

    geral

--- resources/views/post/show-post.blade.php
+++ resources/views/post/show-post.blade.php
@@ -1,43 +1,47 @@
 @extends('layouts.main')
-
-
 @section('content')
 @section('title', $post->title)
-<div class="big-text m-flex">
-    <p>{{ $post->content}}</p>
-</div>
-<p>Esse artigo foi acessado {{$post->visits}} vezes </p>
-<h3>Comentários</h3>
-
-<ul class="content-coments m-flex">
-
-    @forelse ( $post->comments as $comment)
-    <div class="coment-info m-flex">
-        <p>{{ $comment->user->name}} disse: {{ $comment->message}}</p>
-        @if (auth()->check() && auth()->user()->id == $comment->user->id)
-        <form  action="/comments/{{ $comment->id}}" method="post">
-            @csrf
-            @method('delete')
-            <button  class="btn" type="submit"><img src="" alt=""><img src="../assets/image/trash.svg" alt=""></button>
-        </form>
-        @endif
+<div class="container">
+    <div class="card purple shadow-cont" style="max-width: 95%">
+        <span>{{ $post->content}}</span>
     </div>
+    <h1>Comentários</h1>
+    <div class="message">
+        @forelse ( $post->comments as $comment)
+        <div class="itens-column">
+            <div class="text-column">
+                <h4>{{ $comment->user->name}}:</h4>
+                <p>{{ $comment->message}}</p>
+                <div>
+                    @if (auth()->check() && auth()->user()->id == $comment->user->id)
+                    <form  action="/comments/{{ $comment->id}}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button  class="btn" type="submit"><img src="" alt=""><img src="../assets/image/trash.svg" alt=""></button>
+                    </form>
+                    @endif
+                </div>
+            </div>
         @empty
+        </div>
+    </div>
+    <div class="message">
         <p>Não existem comentários cadastrados</p>
+    </div>
     @endforelse
-</ul>
-<div class="form-box">
-    <form action="/comments"  method="POST">
-        @csrf
-        <input type="hidden" name="post_id" value="{{$post->id}}">
+    <div class="show" >
+        <form action="/comments"  method="POST">
+            @csrf
+            <input type="hidden" name="post_id" value="{{$post->id}}">
 
-        <div class="item">
-            <textarea  id="" name="message" cols="30" rows="10" placeholder="Adicione um comentário"></textarea>
-        </div>
-        <div class="card-form-footer">
-            <button class="btn big"><a href="/">Voltar</a></button>
-            <button class="btn big" type="submit">Salvar</button>
-        </div>
-    </form>
+            <div class="item-form">
+                <textarea  id="" name="message" cols="30" rows="10" placeholder="Adicione um comentário"></textarea>
+            </div>
+            <div class="card-form-footer">
+                <button class="btn big"><a href="/">Voltar</a></button>
+                <button class="btn big" type="submit">Salvar</button>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
